ad6b272a0f8cfb7eb58f52a0bf6e0527c453b8fe
hianka <hiankawok@gmail.com>
2022-08-18 16:19:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,7 @@ streamlit.text('🥣 Omega 3 & Blueberry oatmel')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
                 
 
